2de3d73248ea48c254e78c5d650d6ba1aa1c18a0
el-maurete <el-maurete@github.com>
2023-12-17 19:44:32 +0000

    Create Velocicapture app

---
+++ Velocicapture/Data.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Blazored.LocalStorage;
+
+namespace Velocicapture;
+
+public class Storage
+{
+    private readonly ISyncLocalStorageService _localStorageService;
+
+    public Storage(ISyncLocalStorageService localStorageService) =>
+      _localStorageService = localStorageService;
+
+    public Team? LoadTeam()
+    {
+        if (_localStorageService.ContainKey("team"))
+           return _localStorageService.GetItem<Team>("team");
+        return null;
+    }
+
+    public void Store(Team? team)
+    {
+        if (team == null)
+            _localStorageService.RemoveItem("team");
+         else
+            _localStorageService.SetItem("team", team);
+    }
+
+    public List<Sprint> LoadSprints()
+    {
+        if (_localStorageService.ContainKey("sprints"))
+           return _localStorageService.GetItem<List<Sprint>>("sprints");
+        return [];
+    }
+
+    public void Store(List<Sprint>? sprints)
+    {
+        if (sprints == null)
+            _localStorageService.RemoveItem("sprints");
+        _localStorageService.SetItem("sprints", sprints);
+    }
+}
---
+++ Velocicapture/Models.cs
@@ -0,0 +1,40 @@
+﻿namespace Velocicapture;
+
+public class Sprint() 
+{
+    public string Name { get; set; }
+    public int Points { get; set; }
+    public decimal DevDays { get; set; }
+};
+
+public record SprintStats(Sprint Sprint, decimal DevDaysPerSprint)
+{
+    public string Name => Sprint.Name;
+    public int Points => Sprint.Points;
+    public decimal DevDays => Sprint.DevDays;
+    public decimal PointsPerDevDay => Sprint.Points / Sprint.DevDays;
+    public decimal PotentialPoints => Math.Round(PointsPerDevDay * DevDaysPerSprint);
+}
+
+public record SprintPlan(DevPlan[] DevPlans) {
+    public static SprintPlan New(Team team) => new (team.Members.Select(x => new DevPlan(x.Name) { DevDays = 8}).ToArray()); 
+};
+
+public record DevPlan(string DevName) {
+    public int DevDays { get; set; }
+}
+
+public class Team
+{
+    public string Name { get; set; } = "";
+    public int SprintDays { get; set;} = 8;
+    public List<TeamMember> Members { get; set; } = new ();
+    
+    public int SprintDevDays => SprintDays * Members.Count;
+}
+
+public class TeamMember
+{
+    public TeamMember(string name) => Name = name;
+    public string Name { get; set; }
+};
