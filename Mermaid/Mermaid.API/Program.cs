eb881db132f51b9551bd493c17f0e9e772064850
el-maurete <el-maurete@github.com>
2024-02-25 18:58:09 +0000

    Add a background loop to send SignalR messages every 10 seconds

--- Mermaid/Mermaid.API/Program.cs
+++ Mermaid/Mermaid.API/Program.cs
@@ -2,6 +2,7 @@ using Mermaid.Shared;
 using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHostedService<StateRotation>();
 builder.Services.AddSignalR();
 builder.Services.AddCors(options => options.AddPolicy("AllowAll", p => p
   .AllowAnyOrigin()
@@ -12,6 +13,25 @@ var app = builder.Build();
 app.UseCors("AllowAll");
 app.MapHub<MyTestHub>("/chathub");
 app.Run();
+
+class StateRotation : BackgroundService
+{
+    private readonly IHubContext<MyTestHub> _hub;
+
+    public StateRotation(IHubContext<MyTestHub> hub) => _hub = hub;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var states = new string[] { "A", "B", "C", "D" };
+        var x = 0;
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+            x = ++x % states.Length;
+            await _hub.Clients.All.SendAsync(nameof(MyTestHub.ReceiveMessage), states[x]);
+        }
+    }
+}
 
 class MyTestHub : Hub, IServerHub, IClientHub
 {
